e91ba137dfc6d90fd849f53563a46624eb0b5a92
takez0_o <takez0_o@protonmail.com>
2023-11-29 20:19:40 +0300

    gaslimitx2

--- examples/helper/tx.go
+++ examples/helper/tx.go
@@ -10,7 +10,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-// SendTx Send a real transaction to the blockchain.
 func SendTX(client *ethclient.Client, toAddress common.Address, value *big.Int,
 	data []byte, w *Wallet) (*types.Transaction, error) {
 	signedTx, err := TryTX(client, toAddress, value, data, w)
@@ -20,14 +19,12 @@ func SendTX(client *ethclient.Client, toAddress common.Address, value *big.Int,
 	return signedTx, client.SendTransaction(context.Background(), signedTx)
 }
 
-// Trytx Trying to send a transaction, it just return the transaction hash if success.
 func TryTX(client *ethclient.Client, toAddress common.Address, value *big.Int,
 	data []byte, w *Wallet) (*types.Transaction, error) {
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return nil, err
 	}
-
 	gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
 		From:     w.PublicKey,
 		To:       &toAddress,
@@ -45,16 +42,13 @@ func TryTX(client *ethclient.Client, toAddress common.Address, value *big.Int,
 	}
 	tx := types.NewTransaction(nounc, toAddress, value,
 		gasLimit, gasPrice, data)
-
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
 		return nil, err
 	}
-
 	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), w.PrivateKey)
 	if err != nil {
 		return nil, err
 	}
-
 	return signedTx, nil
 }
